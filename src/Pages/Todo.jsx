293c260fa9ce1d4f8b444a06d66940be30504d48
İraz Hazal İlik <ilikhazal16@gmail.com>
2025-08-06 14:57:17 +0000

    Implement Todo component with task addition and display functionality.

--- src/Pages/Todo.jsx
+++ src/Pages/Todo.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { useState } from 'react'
 
 function Todo() {
+  const[task, setTask] = useState('')
+  const[tasks, setTasks] = useState([]) //array to hold tasks
+  const handleAddTask = () => {
+    if(task.trim() === '')
+      return; //prevent adding empty tasks
+    setTasks([...tasks, task]); //add task to the array
+    setTask(''); //clear input field
+  }
   return (
-    <div>Todo</div>
+    <div>
+      <h1>TO-DO </h1>
+      <input
+        type='text'
+        placeholder='Add a new task'
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+      <button onClick={handleAddTask}>Add Task</button>
+      {/* Display the list of tasks */}
+      <ul>
+        {tasks.map((text, key)=> (
+          <li key={key}>
+            {text}
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
