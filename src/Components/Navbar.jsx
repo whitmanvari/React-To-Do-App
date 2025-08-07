9a077fc4a4e0051ed87f830b148bf8f1ee247584
İraz Hazal İlik <ilikhazal16@gmail.com>
2025-08-07 16:27:41 +0000

    Updates scss pages.

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,12 +3,29 @@ import {Link} from 'react-router-dom'
 
 function Navbar() {
   return (
-    <nav>
-        <ul>
-            <li> <Link to ="/">Register</Link></li>
-            <li> <Link to="/login">Login</Link></li>
-            <li> <Link to="/todo">To-do</Link></li>
-            <li> <Link to="/mood">Mood</Link></li>
+    <nav className='navbar'>
+        <div className='navbar-brand'> To-Do App </div>
+        <ul className='navbar-links'>
+        <li>
+            <NavLink to="/" end className={({ isActive }) => (isActive ? 'active' : '')}>
+            Register
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/login" className={({ isActive }) => (isActive ? 'active' : '')}>
+            Login
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/todo" className={({ isActive }) => (isActive ? 'active' : '')}>
+            To-do
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/mood" className={({ isActive }) => (isActive ? 'active' : '')}>
+            Mood
+          </NavLink>
+        </li>
         </ul>
     </nav>
   )
