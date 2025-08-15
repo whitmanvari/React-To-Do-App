b9ace80fdc4ec854ed97c48f4fdc182eb6031886
İraz Hazal İlik <ilikhazal16@gmail.com>
2025-08-15 14:38:41 +0000

    Installed redux/toolkit to register and login page.

---
+++ src/Components/MoodSelector.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function MoodSelector() {
+  return (
+    <div>MoodSelector</div>
+  )
+}
+
+export default MoodSelector
--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import {useState} from 'react'
 
 function Register() {
+  const[formData, setformData]=useState({
+    name:'',
+    email: '',
+    password: ''
+  })
+
+  const handleChange = (e) => {
+    setformData({
+      ...formData, [e.target.name]: e.target.value
+    })
+  }
+  const handleSumbit = (e) => {
+    e.preventDefault()
+    console.log('Kayıt Verileri: ', formData)
+    //API 
+    alert('Kayıt Başarılı!');
+  }
   return (
-    <div>Register</div>
+    <div>
+      <form>
+        <h2>Register</h2>
+
+        <label>Name: </label>
+        <input type='text' name='name' value={formData.name} onChange={{handleChange}}/>
+        <label>E-Mail: </label>
+        <input type='email' name='email' value={formData.email} onChange={handleChange}/>
+        <label>Password: </label>
+        <input type='password' name='password' value={formData.password} onChange={{handleChange}}/>
+        <button type='sumbit'>Register!</button>
+      </form>
+    </div>
   )
 }
 
